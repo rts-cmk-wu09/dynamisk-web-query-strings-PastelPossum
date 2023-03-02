38f5a3e90403d7e20ee6adf5c6248cbf47f8f4f4
PastelPossum <iggyskywalker@gmail.com>
2023-03-02 09:47:39 +0100

    add fetch to destination.html in details.js

---
+++ details.js
@@ -0,0 +1,32 @@
+//Hjælper med at hente parametre fra URLen
+const params = new URLSearchParams(document.location.search)
+const id = params.get("id")
+
+fetch(`./data/${id}.json`)
+    .then(response => response.json())
+    .then((data) => {
+        // console.log(data)
+
+        document.querySelector("body").innerHTML += `
+
+        <img src="/img/${data.image}" alt="">
+        
+        <section class="des-info">
+        <h2>${data.destination}</h2>
+        <h3 class="title">${data.title}</h3>
+        <h3 class="subtitle">${data.subtitle}</h3>
+        <p class="basic-info">${data.text}</p>
+        <h3 class="list-headline">Facilities</h3>
+        <ul class="facilities">
+            <li>${data.facilities[0]}</li>
+            <li>${data.facilities[1]}</li>
+            <li>${data.facilities[2]}</li>
+            <li>${data.facilities[3]}</li>
+        </ul>
+    </section>
+        
+        `
+
+    })
+
+   
