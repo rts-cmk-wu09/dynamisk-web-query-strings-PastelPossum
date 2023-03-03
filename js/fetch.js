15c5754fc7309f7ec9ac8983178221f46aac505c
PastelPossum <iggyskywalker@gmail.com>
2023-03-03 12:13:34 +0100

    outcomment console log

--- js/fetch.js
+++ js/fetch.js
@@ -4,7 +4,7 @@
 // Fetch retunerer et promise. Man bruger en promise handler metode til at arbejde med den. I denne opgave bruger vi .then()
 
 const destinationCards = document.querySelector(".cards")
-console.log(document.querySelector(".cards"))
+// console.log(document.querySelector(".cards"))
 
 // Henter destination json filen - request - får et response tilbage
 //Vi skal konvertere vores response til noget vi kender (json i denne situation)
@@ -13,7 +13,7 @@ fetch("./data/destinations.json")
     //Der bliver herved sendt et nyt promise som vi kan fange hernede
     .then((data) => {
     //Man kan tjekke om man har fået fat i den rigtige data ved at bruge en console log
-    console.log(data);
+    // console.log(data);
     
         data.destinations.forEach((destination) => {
 
