ac50db444737056fe94c4baa56a19836ab34dcd1
PastelPossum <iggyskywalker@gmail.com>
2023-03-03 12:13:23 +0100

    add favourite function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 document.querySelector(".cards").addEventListener("click", (e) => {
-
-   if (e.target.classList.contains("fa-heart")){
-
     
+    if (e.target.classList.contains("fa-heart")){
+        
+        let destinationsId = e.target.closest("div").attributes.id.value
+
+    if(!localStorage.getItem(destinationsId, destinationsId)) {
+
+        localStorage.setItem(destinationsId, destinationsId)
+
+        e.target.classList.add("fa-solid")
+
+     } else {
+
+        localStorage.removeItem(destinationsId)
+
+        e.target.classList.remove("fa-solid")
+        e.target.classList.add("fa-regular")
+
+    }
 
    }
 
